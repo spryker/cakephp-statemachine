f80fb8ffdea7d611a4de898a266fadbc7b34b546
gechetspr <serhii.chepela@spryker.com>
2025-08-06 19:02:36 +0300

    Test classes fix

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -2,86 +2,86 @@
 /**
  * @license http://www.opensource.org/licenses/mit-license.php MIT License
  */
-
-if (!defined('DS')) {
-    define('DS', DIRECTORY_SEPARATOR);
-}
-define('ROOT', dirname(__DIR__));
-define('APP_DIR', 'src');
-
-define('APP', rtrim(sys_get_temp_dir(), DS) . DS . APP_DIR . DS);
-if (!is_dir(APP)) {
-    mkdir(APP, 0770, true);
-}
-
-define('TMP', ROOT . DS . 'tmp' . DS);
-if (!is_dir(TMP)) {
-    mkdir(TMP, 0770, true);
-}
-define('TESTS', ROOT . DS . 'tests' . DS);
-define('CONFIG', TESTS . 'config' . DS);
-
-define('LOGS', TMP . 'logs' . DS);
-define('CACHE', TMP . 'cache' . DS);
-
-define('CAKE_CORE_INCLUDE_PATH', ROOT . '/vendor/cakephp/cakephp');
-define('CORE_PATH', CAKE_CORE_INCLUDE_PATH . DS);
-define('CAKE', CORE_PATH . APP_DIR . DS);
-
-define('WWW_ROOT', TMP . 'webroot' . DS);
-
-require dirname(__DIR__) . '/vendor/autoload.php';
-require CORE_PATH . 'config/bootstrap.php';
-
-Cake\Core\Configure::write('debug', true);
-
-Cake\Core\Configure::write('App', [
-    'namespace' => 'TestApp',
-    'encoding' => 'UTF-8',
-    'paths' => [
-        'templates' => [TESTS . 'test_app' . DS . 'templates' . DS],
-    ],
-]);
-
-Cake\Core\Configure::write('debug', true);
-
-Cake\Core\Configure::write('Transltr.live', getenv('TRANSLTR_LIVE') ?: null);
-
-$cache = [
-    'default' => [
-        'className' => 'File',
-    ],
-    '_cake_core_' => [
-        'className' => 'File',
-        'prefix' => 'crud_myapp_cake_core_',
-        'path' => CACHE . 'persistent/',
-        'serialize' => true,
-        'duration' => '+10 seconds',
-    ],
-    '_cake_model_' => [
-        'className' => 'File',
-        'prefix' => 'crud_my_app_cake_model_',
-        'path' => CACHE . 'models/',
-        'serialize' => 'File',
-        'duration' => '+10 seconds',
-    ],
-];
-
-Cake\Cache\Cache::setConfig($cache);
-
-class_alias(TestApp\Controller\AppController::class, 'App\Controller\AppController');
-class_alias(Cake\View\View::class, 'App\View\AppView');
-class_alias(TestApp\Application::class, 'App\Application');
-
-Cake\Core\Plugin::getCollection()->add(new \StateMachine\StateMachinePlugin());
-
-
-$dbUrl = getenv('DB_DSN') ?: getenv('DB_URL');
-if ($dbUrl) {
-    $config = [
-        'url' => $dbUrl,
-        'quoteIdentifiers' => true,
-    ];
-    \Cake\Datasource\ConnectionManager::drop('test');
-    \Cake\Datasource\ConnectionManager::setConfig('test', $config);
-}
+//
+//if (!defined('DS')) {
+//    define('DS', DIRECTORY_SEPARATOR);
+//}
+//define('ROOT', dirname(__DIR__));
+//define('APP_DIR', 'src');
+//
+//define('APP', rtrim(sys_get_temp_dir(), DS) . DS . APP_DIR . DS);
+//if (!is_dir(APP)) {
+//    mkdir(APP, 0770, true);
+//}
+//
+//define('TMP', ROOT . DS . 'tmp' . DS);
+//if (!is_dir(TMP)) {
+//    mkdir(TMP, 0770, true);
+//}
+//define('TESTS', ROOT . DS . 'tests' . DS);
+//define('CONFIG', TESTS . 'config' . DS);
+//
+//define('LOGS', TMP . 'logs' . DS);
+//define('CACHE', TMP . 'cache' . DS);
+//
+//define('CAKE_CORE_INCLUDE_PATH', ROOT . '/vendor/cakephp/cakephp');
+//define('CORE_PATH', CAKE_CORE_INCLUDE_PATH . DS);
+//define('CAKE', CORE_PATH . APP_DIR . DS);
+//
+//define('WWW_ROOT', TMP . 'webroot' . DS);
+//
+//require dirname(__DIR__) . '/vendor/autoload.php';
+//require CORE_PATH . 'config/bootstrap.php';
+//
+//Cake\Core\Configure::write('debug', true);
+//
+//Cake\Core\Configure::write('App', [
+//    'namespace' => 'TestApp',
+//    'encoding' => 'UTF-8',
+//    'paths' => [
+//        'templates' => [TESTS . 'test_app' . DS . 'templates' . DS],
+//    ],
+//]);
+//
+//Cake\Core\Configure::write('debug', true);
+//
+//Cake\Core\Configure::write('Transltr.live', getenv('TRANSLTR_LIVE') ?: null);
+//
+//$cache = [
+//    'default' => [
+//        'className' => 'File',
+//    ],
+//    '_cake_core_' => [
+//        'className' => 'File',
+//        'prefix' => 'crud_myapp_cake_core_',
+//        'path' => CACHE . 'persistent/',
+//        'serialize' => true,
+//        'duration' => '+10 seconds',
+//    ],
+//    '_cake_model_' => [
+//        'className' => 'File',
+//        'prefix' => 'crud_my_app_cake_model_',
+//        'path' => CACHE . 'models/',
+//        'serialize' => 'File',
+//        'duration' => '+10 seconds',
+//    ],
+//];
+//
+//Cake\Cache\Cache::setConfig($cache);
+//
+//class_alias(TestApp\Controller\AppController::class, 'App\Controller\AppController');
+//class_alias(Cake\View\View::class, 'App\View\AppView');
+//class_alias(TestApp\Application::class, 'App\Application');
+//
+//Cake\Core\Plugin::getCollection()->add(new \StateMachine\StateMachinePlugin());
+//
+//
+//$dbUrl = getenv('DB_DSN') ?: getenv('DB_URL');
+//if ($dbUrl) {
+//    $config = [
+//        'url' => $dbUrl,
+//        'quoteIdentifiers' => true,
+//    ];
+//    \Cake\Datasource\ConnectionManager::drop('test');
+//    \Cake\Datasource\ConnectionManager::setConfig('test', $config);
+//}
--- tests/shim.php
+++ tests/shim.php
@@ -1,18 +1,18 @@
 <?php
 
-use PHP_CodeSniffer\Config;
-
-$manualAutoload = getcwd() . '/vendor/squizlabs/php_codesniffer/autoload.php';
-if (!class_exists(Config::class) && file_exists($manualAutoload)) {
-    require $manualAutoload;
-}
-
-error_reporting(E_ALL & ~E_USER_DEPRECATED);
-
-if (!defined('T_NULLABLE')) {
-    define('T_NULLABLE', 'PHPCS_T_NULLABLE');
-}
-
-if (!class_exists(\PHPUnit\Framework\TestCase::class)) {
-    require 'TestCase.php';
-}
+//use PHP_CodeSniffer\Config;
+//
+//$manualAutoload = getcwd() . '/vendor/squizlabs/php_codesniffer/autoload.php';
+//if (!class_exists(Config::class) && file_exists($manualAutoload)) {
+//    require $manualAutoload;
+//}
+//
+//error_reporting(E_ALL & ~E_USER_DEPRECATED);
+//
+//if (!defined('T_NULLABLE')) {
+//    define('T_NULLABLE', 'PHPCS_T_NULLABLE');
+//}
+//
+//if (!class_exists(\PHPUnit\Framework\TestCase::class)) {
+//    require 'TestCase.php';
+//}
